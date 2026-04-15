5185b7b6b52adaef8c5fcca48bd4f3cbf8989968
Amaury <amauryfranssen@gmail.com>
2026-04-15 15:46:38 +0200

    feat(home): home page layout set --wip

--- app/[lng]/page.tsx
+++ app/[lng]/page.tsx
@@ -21,23 +21,22 @@ export default async function Home({ params }: PageProps) {
   const { t } = await getT("common", { lng });
 
   return (
-    <main className="flex min-h-[calc(100vh-(var(--header-height)+var(--footer-height)))] w-full flex-col">
-      <div className="section-container">
-        <div>
-           
-          <h1 className="text-accent fs-500 ff-sans-cond uppercase letter-spacing-1">
-            So, you want to travel to
-            <span className="block fs-900 ff-serif text-white text-left">Space</span>
-          </h1>
-            
-          <p>Let’s face it; if you want to go to space, you might as well genuinely go to 
-          outer space and not hover kind of on the edge of it. Well sit back, and relax 
-          because we’ll give you a truly out of this world experience! </p>
-        </div>
-          <div>
-            <a href="#" className="space-button uppercase ff-serif fs-600 text-dark bg-white">Explore</a>
+   
+      <main className="main-container main-container--bottom">
+          <div className="">
+            <h1 className="text-accent fs-500 ff-sans-cond uppercase letter-spacing-1 text-center md:text-left">
+              So, you want to travel to
+              <span className="block fs-900 ff-serif text-white text-center md:text-left">Space</span>
+            </h1>
+              
+            <p className="max-w-[50ch]">Let’s face it; if you want to go to space, you might as well genuinely go to 
+            outer space and not hover kind of on the edge of it. Well sit back, and relax 
+            because we’ll give you a truly out of this world experience! </p>
           </div>
-        </div>
-    </main>
+          <div>
+              <a href="#" className="space-button uppercase ff-serif fs-600 text-dark bg-white">Explore</a>
+          </div>
+        </main>
+    
   );
 }
